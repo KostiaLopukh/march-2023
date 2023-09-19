f9f33bd1e02236f04f34ad95e3d174f3dfb5b18c
feden2906 <feden2906@gmail.com>
2023-09-19 21:47:12 +0300

    hw 1

--- app.js
+++ app.js
@@ -1,58 +1,33 @@
-// main.js app.js server.js index.js //
-
-// Modules
-// const { sayHello } = require('./folder/sayHello');
-// sayHello();
-
-// Global variables
-// console.log('FROM APP.JS')
-// console.log('dirname', __dirname);
-// console.log('filename', __filename);
-// console.log('process cwd', process.cwd());
-
-// Path
-// const path = require('node:path');
-// Users/l4pukhh/WebstormProjects/march-2023 - Unix systems
-// D:\\l4pukhh\\WebstormProjects\\march-2023 - Windows
-
-// const joinedPath = path.join(__dirname, 'folder', 'sayHello.js')
-// const normalizedPath = path.normalize('////test///test2///test23///////test45');
-// const resolvedPath = path.resolve('folder', 'sayHello.js');
-
-// OS
-// const os = require('os');
-// console.log(os.cpus());
-// console.log(os.arch());
-// console.log(os.release());
-
-// FS
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
 const path = require('node:path');
 
+const foo = async () => {
+    try {
+        const filesArr = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
+        const folderArr = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
 
-const filePath = path.join(__dirname, 'folder2')
-// fs.writeFile(filePath, 'Hello from Okten !!!', (err) => {
-//   if (err) throw new Error(err.message);
-// })
-// fs.appendFile(filePath, 'hello again\n', (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-// fs.truncate(filePath, (err)=>{
-//   if (err) throw new Error(err.message);
-// })
-// fs.unlink(filePath, (err)=>{
-//   if (err) throw new Error(err.message);
-// });
-// fs.readFile(filePath, {encoding: 'utf-8'}, (err, data) => {
-//   if (err) throw new Error(err.message);
-//   console.log(data);
-// })
-// fs.readdir(filePath, (err, files)=>{
-//   console.log(files);
-// })
-// fs.mkdir(filePath, (err)=>{})
-// fs.rmdir(filePath, (err)=>{})
+        const basePath = path.join(process.cwd(), 'base-folder');
 
+        await fs.mkdir(basePath, { recursive: true });
+        await Promise.all([
+            ...folderArr.map(async (folderName) => {
+                await fs.mkdir(path.join(basePath, folderName));
+            }),
+            ...filesArr.map(async (fileName) => {
+                await fs.writeFile(path.join(basePath, fileName), 'HELLO !!!');
+            })
+        ]);
 
+        const arr = await fs.readdir(basePath);
 
+        await Promise.all(arr.map(async (item) => {
+            const stat = await fs.stat(path.join(basePath, item));
+            console.log(stat.isFile() ? 'FILE: ' : 'FOLDER: ', item);
+        }));
 
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+foo().then();
