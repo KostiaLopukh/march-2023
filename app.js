cf63ab96d587acd370f5f4428507d7858938864d
l4pukh <k0styalopuh3@gmail.com>
2023-09-20 21:25:40 +0300

    events, streams, express

--- app.js
+++ app.js
@@ -1,33 +1,116 @@
-const fs = require('node:fs/promises');
-const path = require('node:path');
+// const Events = require('node:events');
+//
+// const eventEmitter = new Events();
+//
+// eventEmitter.on('click', ()=>{
+//   console.log('Click click click')
+// });
+//
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+// eventEmitter.emit('click');
+//
+//
+// eventEmitter.once('sayHello', ()=>{
+//   console.log('Hello hello hello');
+// });
+//
+// console.log(eventEmitter.eventNames())
+//
+// eventEmitter.emit('sayHello') // sayHello
+// eventEmitter.emit('sayHello') // event does not exist
+// eventEmitter.emit('sayHello')
+// eventEmitter.emit('sayHello')
+//
+// console.log(eventEmitter.eventNames());
+//
+// const fs = require('fs');
+// const path = require('path');
+//
+//
+// const readStream = fs.createReadStream(path.join(__dirname, 'folder', 'text3.txt'), { highWaterMark: 10000 });
+// const writeStream = fs.createWriteStream(path.join(__dirname, 'folder', 'text2.txt'));
+//
+// readStream.on('data', (chunk)=>{
+//   console.log(chunk);
+//   writeStream.write(chunk);
+// });
+//
+// readStream.pipe(writeStream);
+//
+// readStream.on('error', ()=>{
+//   console.log('error happened');
+//   readStream.destroy();
+//   writeStream.end('ERROR HAPPENED');
+// })
 
-const foo = async () => {
-    try {
-        const filesArr = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt', 'file5.txt'];
-        const folderArr = ['folder1', 'folder2', 'folder3', 'folder4', 'folder5'];
+const express = require('express');
 
-        const basePath = path.join(process.cwd(), 'base-folder');
+const app = express();
 
-        await fs.mkdir(basePath, { recursive: true });
-        await Promise.all([
-            ...folderArr.map(async (folderName) => {
-                await fs.mkdir(path.join(basePath, folderName));
-            }),
-            ...filesArr.map(async (fileName) => {
-                await fs.writeFile(path.join(basePath, fileName), 'HELLO !!!');
-            })
-        ]);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-        const arr = await fs.readdir(basePath);
+const users = [
+  { id: 1, name: 'Іван', email: 'ivan@example.com' },
+  { id: 2, name: 'Марія', email: 'maria@example.com' },
+  { id: 3, name: 'Петро', email: 'petro@example.com' },
+  { id: 4, name: 'Ольга', email: 'olga@example.com' },
+  { id: 5, name: 'Андрій', email: 'andriy@example.com' },
+  { id: 6, name: 'Наталія', email: 'natalia@example.com' },
+  { id: 7, name: 'Максим', email: 'maxim@example.com' },
+  { id: 8, name: 'Софія', email: 'sofia@example.com' },
+  { id: 9, name: 'Анна', email: 'anna@example.com' },
+  { id: 10, name: 'Олександр', email: 'oleksandr@example.com' }
+];
 
-        await Promise.all(arr.map(async (item) => {
-            const stat = await fs.stat(path.join(basePath, item));
-            console.log(stat.isFile() ? 'FILE: ' : 'FOLDER: ', item);
-        }));
+app.get('/users', (req, res)=>{
+  res.json({
+    data: users,
+  })
+})
 
-    } catch (e) {
-        console.error(e.message);
-    }
-}
+app.get('/users/:id', (req, res)=>{
+  const { id } = req.params;
 
-foo().then();
+  res.json({
+    data: users[+id - 1],
+  })
+})
+
+app.post('/users', (req, res)=>{
+  users.push(req.body);
+
+  res.status(201).json({
+    message: "User created",
+  });
+})
+
+app.delete('/users/:id', (req, res)=>{
+  const { id } = req.params;
+
+  users.splice(+id - 1, 1);
+
+  res.sendStatus(204);
+})
+
+app.put('/users/:id', (req, res)=>{
+  const { id } = req.params;
+
+  users[id] = req.body;
+
+  res.json({
+    message: 'User updated',
+  })
+})
+
+const PORT = 5001;
+
+app.listen(PORT, ()=>{
+  console.log(`Server has successfully started on PORT ${PORT}`);
+})
+
+
+// CRUD c - create, r - read, u - update, d - delete
+
